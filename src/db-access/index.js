8e52527b377cea6bb63b9d90033d2a9e6b5990dd
guiyep <guiyep@gmail.com>
2022-04-14 16:22:15 +1000

    first commit

--- src/db-access/index.js
+++ src/db-access/index.js
@@ -1,22 +1,21 @@
 import localforage from 'localforage';
 
-export const createStore = ({ name }) => {
-  return localforage.createInstance({
+export const createStore = ({ name }) =>
+  localforage.createInstance({
     driver: localforage.INDEXEDDB,
     name,
   });
-};
 
-export const get = ({ key }, Store) => Store.getItem('key');
+export const get = ({ key }, Store) => Store.getItem(key);
 
 export const set = async ({ key, value }, Store) => {
   await Store.setItem(key, value);
-  return getItem(key);
+  return get(key);
 };
 
 export const find = async (condition, Store) => {
   const result = [];
-  await Store.iterate((value, key, iterationNumber) => {
+  await Store.iterate((value, key) => {
     if (condition({ value, key })) {
       result.push({ value, key });
     }
@@ -28,7 +27,7 @@ export const DbAccess = ({ name }) => {
   const Store = createStore({ name });
   return {
     get: ({ key }) => get({ key }, Store),
-    set: ({ key, value }) => set({ key }, Store),
+    set: ({ key, value }) => set({ key, value }, Store),
     find: (condition) => find(condition, Store),
   };
 };
